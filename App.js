a298945fc45e8219e6fef53e34c6e4f148a4f0bb
fatihcaliss <fthcls@gmail.com>
2023-12-26 17:17:54 +0300

    add expo-av and sounds

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button } from "react-native";
+import { Audio } from "expo-av";
+import { useEffect, useState } from "react";
+
+const dogSound = require("./assets/barking_dogs.wav");
+const birdSound = require("./assets/bird.wav");
+const horseSound = require("./assets/horse.wav");
+const catSound = require("./assets/cat.wav");
 
 export default function App() {
+  const [sound, setSound] = useState();
+  // const [soundUrl, setsoundUrl] = useState();
+
+  async function playSound(arg) {
+    console.log("Loading Sound");
+    const test = arg || "";
+    const { sound } = await Audio.Sound.createAsync(test);
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  function stopSounds() {
+    sound.unloadAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Play Bird Sound" onPress={() => playSound(birdSound)} />
+      <Button title="Play Dog Sound" onPress={() => playSound(dogSound)} />
+      <Button title="Play Horse Sound" onPress={() => playSound(horseSound)} />
+      <Button title="Play Cat Sound" onPress={() => playSound(catSound)} />
+      <Button title="Stop" onPress={stopSounds} />
     </View>
   );
 }
@@ -13,8 +48,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 10,
   },
 });
