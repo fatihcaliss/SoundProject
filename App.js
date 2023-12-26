7c5e45cf7209b21d0f6ee29750402422f0f5d40e
fatihcaliss <fthcls@gmail.com>
2023-12-26 17:48:54 +0300

    create components

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import { Audio } from "expo-av";
 import { useEffect, useState } from "react";
+import Button from "./components/UI/Button";
+import OutlinedButton from "./components/UI/OutlinedButton";
 
 const dogSound = require("./assets/barking_dogs.wav");
 const birdSound = require("./assets/bird.wav");
@@ -9,7 +11,6 @@ const catSound = require("./assets/cat.wav");
 
 export default function App() {
   const [sound, setSound] = useState();
-  // const [soundUrl, setsoundUrl] = useState();
 
   async function playSound(arg) {
     console.log("Loading Sound");
@@ -36,11 +37,33 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title="Play Bird Sound" onPress={() => playSound(birdSound)} />
-      <Button title="Play Dog Sound" onPress={() => playSound(dogSound)} />
-      <Button title="Play Horse Sound" onPress={() => playSound(horseSound)} />
-      <Button title="Play Cat Sound" onPress={() => playSound(catSound)} />
-      <Button title="Stop" onPress={stopSounds} />
+      <View style={styles.buttonItem}>
+        <Button onPress={() => playSound(birdSound)}>Bird Sound</Button>
+      </View>
+      <View style={styles.buttonItem}>
+        <Button onPress={() => playSound(dogSound)}>Dog Sound</Button>
+      </View>
+      <View style={styles.buttonItem}>
+        <Button onPress={() => playSound(horseSound)}>Horse Sound</Button>
+      </View>
+      <View style={styles.buttonItem}>
+        <Button onPress={() => playSound(catSound)}>Cat Sound</Button>
+      </View>
+      <View style={styles.buttonItem}>
+        <OutlinedButton icon="stop" onPress={stopSounds}>
+          {" "}
+          Stopp{" "}
+        </OutlinedButton>
+      </View>
+
+      {/* <Button onPress={() => playSound(birdSound)}>Bird Sound</Button>
+      <Button onPress={() => playSound(dogSound)}>Dog Sound</Button>
+      <Button onPress={() => playSound(horseSound)}>Horse Sound</Button>
+      <Button onPress={() => playSound(catSound)}>Cat Sound</Button>
+      <OutlinedButton icon="stop" onPress={stopSounds}>
+        {" "}
+        Stopp{" "}
+      </OutlinedButton> */}
     </View>
   );
 }
@@ -51,5 +74,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#ecf0f1",
     padding: 10,
+  },
+  buttonItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 16,
   },
 });
